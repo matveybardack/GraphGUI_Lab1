f73bbdc4be38278c3297692b1f374047702a497b
matveybardack <matbardack@gmail.com>
2026-02-03 22:33:32 +0300

    Вынес общий интерфейс холста с графом в отдельный контроллер

---
+++ WpfAppGraph/Converters/VertexStateToBorderBrushConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAppGraph.Converters
+{
+    class VertexStateToBorderBrushConverter
+    {
+    }
+}
---
+++ WpfAppGraph/Converters/VertexStateToBrushConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfAppGraph.Converters
+{
+    public class VertexStateToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // В реальности будет enum VertexState
+            if (value is string state)
+            {
+                return state switch
+                {
+                    "Selected" => Brushes.Gold,
+                    "Target" => Brushes.LightGreen,
+                    "Visited" => Brushes.LightCoral,
+                    "Current" => Brushes.Orange,
+                    _ => Brushes.LightBlue
+                };
+            }
+            return Brushes.LightBlue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
